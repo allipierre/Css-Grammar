c0521d78e9eccff3eca98815bd157182413437da
allipierre <yottiallipierre40@yahoo.fr>
2017-09-11 12:10:04 +0200

    initial Commit

---
+++ css3BaseListener.java
@@ -0,0 +1,280 @@
+// Generated from css3.g4 by ANTLR 4.0
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.ErrorNode;
+
+public class css3BaseListener implements css3Listener {
+	@Override public void enterSelectorGroup(css3Parser.SelectorGroupContext ctx) { }
+	@Override public void exitSelectorGroup(css3Parser.SelectorGroupContext ctx) { }
+
+	@Override public void enterFontFamilyNameList(css3Parser.FontFamilyNameListContext ctx) { }
+	@Override public void exitFontFamilyNameList(css3Parser.FontFamilyNameListContext ctx) { }
+
+	@Override public void enterUnknownDeclaration(css3Parser.UnknownDeclarationContext ctx) { }
+	@Override public void exitUnknownDeclaration(css3Parser.UnknownDeclarationContext ctx) { }
+
+	@Override public void enterNegation(css3Parser.NegationContext ctx) { }
+	@Override public void exitNegation(css3Parser.NegationContext ctx) { }
+
+	@Override public void enterKnownFontFaceDeclaration(css3Parser.KnownFontFaceDeclarationContext ctx) { }
+	@Override public void exitKnownFontFaceDeclaration(css3Parser.KnownFontFaceDeclarationContext ctx) { }
+
+	@Override public void enterIdent(css3Parser.IdentContext ctx) { }
+	@Override public void exitIdent(css3Parser.IdentContext ctx) { }
+
+	@Override public void enterUnknownFontFaceDeclaration(css3Parser.UnknownFontFaceDeclarationContext ctx) { }
+	@Override public void exitUnknownFontFaceDeclaration(css3Parser.UnknownFontFaceDeclarationContext ctx) { }
+
+	@Override public void enterDeclarationList(css3Parser.DeclarationListContext ctx) { }
+	@Override public void exitDeclarationList(css3Parser.DeclarationListContext ctx) { }
+
+	@Override public void enterGoodCharset(css3Parser.GoodCharsetContext ctx) { }
+	@Override public void exitGoodCharset(css3Parser.GoodCharsetContext ctx) { }
+
+	@Override public void enterGroupRuleBody(css3Parser.GroupRuleBodyContext ctx) { }
+	@Override public void exitGroupRuleBody(css3Parser.GroupRuleBodyContext ctx) { }
+
+	@Override public void enterElementName(css3Parser.ElementNameContext ctx) { }
+	@Override public void exitElementName(css3Parser.ElementNameContext ctx) { }
+
+	@Override public void enterNestedStatement(css3Parser.NestedStatementContext ctx) { }
+	@Override public void exitNestedStatement(css3Parser.NestedStatementContext ctx) { }
+
+	@Override public void enterNumber(css3Parser.NumberContext ctx) { }
+	@Override public void exitNumber(css3Parser.NumberContext ctx) { }
+
+	@Override public void enterAtKeyword(css3Parser.AtKeywordContext ctx) { }
+	@Override public void exitAtKeyword(css3Parser.AtKeywordContext ctx) { }
+
+	@Override public void enterHexcolor(css3Parser.HexcolorContext ctx) { }
+	@Override public void exitHexcolor(css3Parser.HexcolorContext ctx) { }
+
+	@Override public void enterCounterStyle(css3Parser.CounterStyleContext ctx) { }
+	@Override public void exitCounterStyle(css3Parser.CounterStyleContext ctx) { }
+
+	@Override public void enterFunction(css3Parser.FunctionContext ctx) { }
+	@Override public void exitFunction(css3Parser.FunctionContext ctx) { }
+
+	@Override public void enterPercentage(css3Parser.PercentageContext ctx) { }
+	@Override public void exitPercentage(css3Parser.PercentageContext ctx) { }
+
+	@Override public void enterKeyframeSelector(css3Parser.KeyframeSelectorContext ctx) { }
+	@Override public void exitKeyframeSelector(css3Parser.KeyframeSelectorContext ctx) { }
+
+	@Override public void enterCalcSum(css3Parser.CalcSumContext ctx) { }
+	@Override public void exitCalcSum(css3Parser.CalcSumContext ctx) { }
+
+	@Override public void enterBlock(css3Parser.BlockContext ctx) { }
+	@Override public void exitBlock(css3Parser.BlockContext ctx) { }
+
+	@Override public void enterPrio(css3Parser.PrioContext ctx) { }
+	@Override public void exitPrio(css3Parser.PrioContext ctx) { }
+
+	@Override public void enterSupportsConditionInParens(css3Parser.SupportsConditionInParensContext ctx) { }
+	@Override public void exitSupportsConditionInParens(css3Parser.SupportsConditionInParensContext ctx) { }
+
+	@Override public void enterPseudo(css3Parser.PseudoContext ctx) { }
+	@Override public void exitPseudo(css3Parser.PseudoContext ctx) { }
+
+	@Override public void enterMediaQuery(css3Parser.MediaQueryContext ctx) { }
+	@Override public void exitMediaQuery(css3Parser.MediaQueryContext ctx) { }
+
+	@Override public void enterKnownDeclaration(css3Parser.KnownDeclarationContext ctx) { }
+	@Override public void exitKnownDeclaration(css3Parser.KnownDeclarationContext ctx) { }
+
+	@Override public void enterFontFaceRule(css3Parser.FontFaceRuleContext ctx) { }
+	@Override public void exitFontFaceRule(css3Parser.FontFaceRuleContext ctx) { }
+
+	@Override public void enterFeatureValueDefinition(css3Parser.FeatureValueDefinitionContext ctx) { }
+	@Override public void exitFeatureValueDefinition(css3Parser.FeatureValueDefinitionContext ctx) { }
+
+	@Override public void enterStylesheet(css3Parser.StylesheetContext ctx) { }
+	@Override public void exitStylesheet(css3Parser.StylesheetContext ctx) { }
+
+	@Override public void enterTypeSelector(css3Parser.TypeSelectorContext ctx) { }
+	@Override public void exitTypeSelector(css3Parser.TypeSelectorContext ctx) { }
+
+	@Override public void enterUnknownRuleset(css3Parser.UnknownRulesetContext ctx) { }
+	@Override public void exitUnknownRuleset(css3Parser.UnknownRulesetContext ctx) { }
+
+	@Override public void enterCalcProduct(css3Parser.CalcProductContext ctx) { }
+	@Override public void exitCalcProduct(css3Parser.CalcProductContext ctx) { }
+
+	@Override public void enterFeatureValueBlock(css3Parser.FeatureValueBlockContext ctx) { }
+	@Override public void exitFeatureValueBlock(css3Parser.FeatureValueBlockContext ctx) { }
+
+	@Override public void enterVar(css3Parser.VarContext ctx) { }
+	@Override public void exitVar(css3Parser.VarContext ctx) { }
+
+	@Override public void enterMediaType(css3Parser.MediaTypeContext ctx) { }
+	@Override public void exitMediaType(css3Parser.MediaTypeContext ctx) { }
+
+	@Override public void enterFontFeatureValuesRule(css3Parser.FontFeatureValuesRuleContext ctx) { }
+	@Override public void exitFontFeatureValuesRule(css3Parser.FontFeatureValuesRuleContext ctx) { }
+
+	@Override public void enterNamespacePrefix(css3Parser.NamespacePrefixContext ctx) { }
+	@Override public void exitNamespacePrefix(css3Parser.NamespacePrefixContext ctx) { }
+
+	@Override public void enterUnknownAtRule(css3Parser.UnknownAtRuleContext ctx) { }
+	@Override public void exitUnknownAtRule(css3Parser.UnknownAtRuleContext ctx) { }
+
+	@Override public void enterBadImport(css3Parser.BadImportContext ctx) { }
+	@Override public void exitBadImport(css3Parser.BadImportContext ctx) { }
+
+	@Override public void enterFontFamilyName(css3Parser.FontFamilyNameContext ctx) { }
+	@Override public void exitFontFamilyName(css3Parser.FontFamilyNameContext ctx) { }
+
+	@Override public void enterGoodNamespace(css3Parser.GoodNamespaceContext ctx) { }
+	@Override public void exitGoodNamespace(css3Parser.GoodNamespaceContext ctx) { }
+
+	@Override public void enterGeneralEnclosed(css3Parser.GeneralEnclosedContext ctx) { }
+	@Override public void exitGeneralEnclosed(css3Parser.GeneralEnclosedContext ctx) { }
+
+	@Override public void enterBadNamespace(css3Parser.BadNamespaceContext ctx) { }
+	@Override public void exitBadNamespace(css3Parser.BadNamespaceContext ctx) { }
+
+	@Override public void enterPseudoPage(css3Parser.PseudoPageContext ctx) { }
+	@Override public void exitPseudoPage(css3Parser.PseudoPageContext ctx) { }
+
+	@Override public void enterUnknownDimension(css3Parser.UnknownDimensionContext ctx) { }
+	@Override public void exitUnknownDimension(css3Parser.UnknownDimensionContext ctx) { }
+
+	@Override public void enterBadProperty(css3Parser.BadPropertyContext ctx) { }
+	@Override public void exitBadProperty(css3Parser.BadPropertyContext ctx) { }
+
+	@Override public void enterGoodOperator(css3Parser.GoodOperatorContext ctx) { }
+	@Override public void exitGoodOperator(css3Parser.GoodOperatorContext ctx) { }
+
+	@Override public void enterKnownTerm(css3Parser.KnownTermContext ctx) { }
+	@Override public void exitKnownTerm(css3Parser.KnownTermContext ctx) { }
+
+	@Override public void enterUnused(css3Parser.UnusedContext ctx) { }
+	@Override public void exitUnused(css3Parser.UnusedContext ctx) { }
+
+	@Override public void enterPage(css3Parser.PageContext ctx) { }
+	@Override public void exitPage(css3Parser.PageContext ctx) { }
+
+	@Override public void enterKeyframesRule(css3Parser.KeyframesRuleContext ctx) { }
+	@Override public void exitKeyframesRule(css3Parser.KeyframesRuleContext ctx) { }
+
+	@Override public void enterMediaFeature(css3Parser.MediaFeatureContext ctx) { }
+	@Override public void exitMediaFeature(css3Parser.MediaFeatureContext ctx) { }
+
+	@Override public void enterBadTerm(css3Parser.BadTermContext ctx) { }
+	@Override public void exitBadTerm(css3Parser.BadTermContext ctx) { }
+
+	@Override public void enterBadCharset(css3Parser.BadCharsetContext ctx) { }
+	@Override public void exitBadCharset(css3Parser.BadCharsetContext ctx) { }
+
+	@Override public void enterCombinator(css3Parser.CombinatorContext ctx) { }
+	@Override public void exitCombinator(css3Parser.CombinatorContext ctx) { }
+
+	@Override public void enterFunctionalPseudo(css3Parser.FunctionalPseudoContext ctx) { }
+	@Override public void exitFunctionalPseudo(css3Parser.FunctionalPseudoContext ctx) { }
+
+	@Override public void enterSupportsNegation(css3Parser.SupportsNegationContext ctx) { }
+	@Override public void exitSupportsNegation(css3Parser.SupportsNegationContext ctx) { }
+
+	@Override public void enterClassName(css3Parser.ClassNameContext ctx) { }
+	@Override public void exitClassName(css3Parser.ClassNameContext ctx) { }
+
+	@Override public void enterMedia(css3Parser.MediaContext ctx) { }
+	@Override public void exitMedia(css3Parser.MediaContext ctx) { }
+
+	@Override public void enterTypeNamespacePrefix(css3Parser.TypeNamespacePrefixContext ctx) { }
+	@Override public void exitTypeNamespacePrefix(css3Parser.TypeNamespacePrefixContext ctx) { }
+
+	@Override public void enterGoodImport(css3Parser.GoodImportContext ctx) { }
+	@Override public void exitGoodImport(css3Parser.GoodImportContext ctx) { }
+
+	@Override public void enterSupportsCondition(css3Parser.SupportsConditionContext ctx) { }
+	@Override public void exitSupportsCondition(css3Parser.SupportsConditionContext ctx) { }
+
+	@Override public void enterAttrib(css3Parser.AttribContext ctx) { }
+	@Override public void exitAttrib(css3Parser.AttribContext ctx) { }
+
+	@Override public void enterKnownRuleset(css3Parser.KnownRulesetContext ctx) { }
+	@Override public void exitKnownRuleset(css3Parser.KnownRulesetContext ctx) { }
+
+	@Override public void enterFeatureType(css3Parser.FeatureTypeContext ctx) { }
+	@Override public void exitFeatureType(css3Parser.FeatureTypeContext ctx) { }
+
+	@Override public void enterNegationArg(css3Parser.NegationArgContext ctx) { }
+	@Override public void exitNegationArg(css3Parser.NegationArgContext ctx) { }
+
+	@Override public void enterSelector(css3Parser.SelectorContext ctx) { }
+	@Override public void exitSelector(css3Parser.SelectorContext ctx) { }
+
+	@Override public void enterExpr(css3Parser.ExprContext ctx) { }
+	@Override public void exitExpr(css3Parser.ExprContext ctx) { }
+
+	@Override public void enterSupportsRule(css3Parser.SupportsRuleContext ctx) { }
+	@Override public void exitSupportsRule(css3Parser.SupportsRuleContext ctx) { }
+
+	@Override public void enterUnknownTerm(css3Parser.UnknownTermContext ctx) { }
+	@Override public void exitUnknownTerm(css3Parser.UnknownTermContext ctx) { }
+
+	@Override public void enterWs(css3Parser.WsContext ctx) { }
+	@Override public void exitWs(css3Parser.WsContext ctx) { }
+
+	@Override public void enterUniversal(css3Parser.UniversalContext ctx) { }
+	@Override public void exitUniversal(css3Parser.UniversalContext ctx) { }
+
+	@Override public void enterValue(css3Parser.ValueContext ctx) { }
+	@Override public void exitValue(css3Parser.ValueContext ctx) { }
+
+	@Override public void enterDimension(css3Parser.DimensionContext ctx) { }
+	@Override public void exitDimension(css3Parser.DimensionContext ctx) { }
+
+	@Override public void enterDxImageTransform(css3Parser.DxImageTransformContext ctx) { }
+	@Override public void exitDxImageTransform(css3Parser.DxImageTransformContext ctx) { }
+
+	@Override public void enterCalcValue(css3Parser.CalcValueContext ctx) { }
+	@Override public void exitCalcValue(css3Parser.CalcValueContext ctx) { }
+
+	@Override public void enterSupportsDisjunction(css3Parser.SupportsDisjunctionContext ctx) { }
+	@Override public void exitSupportsDisjunction(css3Parser.SupportsDisjunctionContext ctx) { }
+
+	@Override public void enterMediaQueryList(css3Parser.MediaQueryListContext ctx) { }
+	@Override public void exitMediaQueryList(css3Parser.MediaQueryListContext ctx) { }
+
+	@Override public void enterExpression(css3Parser.ExpressionContext ctx) { }
+	@Override public void exitExpression(css3Parser.ExpressionContext ctx) { }
+
+	@Override public void enterSupportsConjunction(css3Parser.SupportsConjunctionContext ctx) { }
+	@Override public void exitSupportsConjunction(css3Parser.SupportsConjunctionContext ctx) { }
+
+	@Override public void enterKeyframesBlocks(css3Parser.KeyframesBlocksContext ctx) { }
+	@Override public void exitKeyframesBlocks(css3Parser.KeyframesBlocksContext ctx) { }
+
+	@Override public void enterBadOperator(css3Parser.BadOperatorContext ctx) { }
+	@Override public void exitBadOperator(css3Parser.BadOperatorContext ctx) { }
+
+	@Override public void enterGoodProperty(css3Parser.GoodPropertyContext ctx) { }
+	@Override public void exitGoodProperty(css3Parser.GoodPropertyContext ctx) { }
+
+	@Override public void enterAny(css3Parser.AnyContext ctx) { }
+	@Override public void exitAny(css3Parser.AnyContext ctx) { }
+
+	@Override public void enterMediaExpression(css3Parser.MediaExpressionContext ctx) { }
+	@Override public void exitMediaExpression(css3Parser.MediaExpressionContext ctx) { }
+
+	@Override public void enterSupportsDeclarationCondition(css3Parser.SupportsDeclarationConditionContext ctx) { }
+	@Override public void exitSupportsDeclarationCondition(css3Parser.SupportsDeclarationConditionContext ctx) { }
+
+	@Override public void enterSimpleSelectorSequence(css3Parser.SimpleSelectorSequenceContext ctx) { }
+	@Override public void exitSimpleSelectorSequence(css3Parser.SimpleSelectorSequenceContext ctx) { }
+
+	@Override public void enterViewport(css3Parser.ViewportContext ctx) { }
+	@Override public void exitViewport(css3Parser.ViewportContext ctx) { }
+
+	@Override public void enterCalc(css3Parser.CalcContext ctx) { }
+	@Override public void exitCalc(css3Parser.CalcContext ctx) { }
+
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	@Override public void visitTerminal(TerminalNode node) { }
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ css3Listener.java
@@ -0,0 +1,272 @@
+// Generated from css3.g4 by ANTLR 4.0
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface css3Listener extends ParseTreeListener {
+	void enterSelectorGroup(css3Parser.SelectorGroupContext ctx);
+	void exitSelectorGroup(css3Parser.SelectorGroupContext ctx);
+
+	void enterFontFamilyNameList(css3Parser.FontFamilyNameListContext ctx);
+	void exitFontFamilyNameList(css3Parser.FontFamilyNameListContext ctx);
+
+	void enterUnknownDeclaration(css3Parser.UnknownDeclarationContext ctx);
+	void exitUnknownDeclaration(css3Parser.UnknownDeclarationContext ctx);
+
+	void enterNegation(css3Parser.NegationContext ctx);
+	void exitNegation(css3Parser.NegationContext ctx);
+
+	void enterKnownFontFaceDeclaration(css3Parser.KnownFontFaceDeclarationContext ctx);
+	void exitKnownFontFaceDeclaration(css3Parser.KnownFontFaceDeclarationContext ctx);
+
+	void enterIdent(css3Parser.IdentContext ctx);
+	void exitIdent(css3Parser.IdentContext ctx);
+
+	void enterUnknownFontFaceDeclaration(css3Parser.UnknownFontFaceDeclarationContext ctx);
+	void exitUnknownFontFaceDeclaration(css3Parser.UnknownFontFaceDeclarationContext ctx);
+
+	void enterDeclarationList(css3Parser.DeclarationListContext ctx);
+	void exitDeclarationList(css3Parser.DeclarationListContext ctx);
+
+	void enterGoodCharset(css3Parser.GoodCharsetContext ctx);
+	void exitGoodCharset(css3Parser.GoodCharsetContext ctx);
+
+	void enterGroupRuleBody(css3Parser.GroupRuleBodyContext ctx);
+	void exitGroupRuleBody(css3Parser.GroupRuleBodyContext ctx);
+
+	void enterElementName(css3Parser.ElementNameContext ctx);
+	void exitElementName(css3Parser.ElementNameContext ctx);
+
+	void enterNestedStatement(css3Parser.NestedStatementContext ctx);
+	void exitNestedStatement(css3Parser.NestedStatementContext ctx);
+
+	void enterNumber(css3Parser.NumberContext ctx);
+	void exitNumber(css3Parser.NumberContext ctx);
+
+	void enterAtKeyword(css3Parser.AtKeywordContext ctx);
+	void exitAtKeyword(css3Parser.AtKeywordContext ctx);
+
+	void enterHexcolor(css3Parser.HexcolorContext ctx);
+	void exitHexcolor(css3Parser.HexcolorContext ctx);
+
+	void enterCounterStyle(css3Parser.CounterStyleContext ctx);
+	void exitCounterStyle(css3Parser.CounterStyleContext ctx);
+
+	void enterFunction(css3Parser.FunctionContext ctx);
+	void exitFunction(css3Parser.FunctionContext ctx);
+
+	void enterPercentage(css3Parser.PercentageContext ctx);
+	void exitPercentage(css3Parser.PercentageContext ctx);
+
+	void enterKeyframeSelector(css3Parser.KeyframeSelectorContext ctx);
+	void exitKeyframeSelector(css3Parser.KeyframeSelectorContext ctx);
+
+	void enterCalcSum(css3Parser.CalcSumContext ctx);
+	void exitCalcSum(css3Parser.CalcSumContext ctx);
+
+	void enterBlock(css3Parser.BlockContext ctx);
+	void exitBlock(css3Parser.BlockContext ctx);
+
+	void enterPrio(css3Parser.PrioContext ctx);
+	void exitPrio(css3Parser.PrioContext ctx);
+
+	void enterSupportsConditionInParens(css3Parser.SupportsConditionInParensContext ctx);
+	void exitSupportsConditionInParens(css3Parser.SupportsConditionInParensContext ctx);
+
+	void enterPseudo(css3Parser.PseudoContext ctx);
+	void exitPseudo(css3Parser.PseudoContext ctx);
+
+	void enterMediaQuery(css3Parser.MediaQueryContext ctx);
+	void exitMediaQuery(css3Parser.MediaQueryContext ctx);
+
+	void enterKnownDeclaration(css3Parser.KnownDeclarationContext ctx);
+	void exitKnownDeclaration(css3Parser.KnownDeclarationContext ctx);
+
+	void enterFontFaceRule(css3Parser.FontFaceRuleContext ctx);
+	void exitFontFaceRule(css3Parser.FontFaceRuleContext ctx);
+
+	void enterFeatureValueDefinition(css3Parser.FeatureValueDefinitionContext ctx);
+	void exitFeatureValueDefinition(css3Parser.FeatureValueDefinitionContext ctx);
+
+	void enterStylesheet(css3Parser.StylesheetContext ctx);
+	void exitStylesheet(css3Parser.StylesheetContext ctx);
+
+	void enterTypeSelector(css3Parser.TypeSelectorContext ctx);
+	void exitTypeSelector(css3Parser.TypeSelectorContext ctx);
+
+	void enterUnknownRuleset(css3Parser.UnknownRulesetContext ctx);
+	void exitUnknownRuleset(css3Parser.UnknownRulesetContext ctx);
+
+	void enterCalcProduct(css3Parser.CalcProductContext ctx);
+	void exitCalcProduct(css3Parser.CalcProductContext ctx);
+
+	void enterFeatureValueBlock(css3Parser.FeatureValueBlockContext ctx);
+	void exitFeatureValueBlock(css3Parser.FeatureValueBlockContext ctx);
+
+	void enterVar(css3Parser.VarContext ctx);
+	void exitVar(css3Parser.VarContext ctx);
+
+	void enterMediaType(css3Parser.MediaTypeContext ctx);
+	void exitMediaType(css3Parser.MediaTypeContext ctx);
+
+	void enterFontFeatureValuesRule(css3Parser.FontFeatureValuesRuleContext ctx);
+	void exitFontFeatureValuesRule(css3Parser.FontFeatureValuesRuleContext ctx);
+
+	void enterNamespacePrefix(css3Parser.NamespacePrefixContext ctx);
+	void exitNamespacePrefix(css3Parser.NamespacePrefixContext ctx);
+
+	void enterUnknownAtRule(css3Parser.UnknownAtRuleContext ctx);
+	void exitUnknownAtRule(css3Parser.UnknownAtRuleContext ctx);
+
+	void enterBadImport(css3Parser.BadImportContext ctx);
+	void exitBadImport(css3Parser.BadImportContext ctx);
+
+	void enterFontFamilyName(css3Parser.FontFamilyNameContext ctx);
+	void exitFontFamilyName(css3Parser.FontFamilyNameContext ctx);
+
+	void enterGoodNamespace(css3Parser.GoodNamespaceContext ctx);
+	void exitGoodNamespace(css3Parser.GoodNamespaceContext ctx);
+
+	void enterGeneralEnclosed(css3Parser.GeneralEnclosedContext ctx);
+	void exitGeneralEnclosed(css3Parser.GeneralEnclosedContext ctx);
+
+	void enterBadNamespace(css3Parser.BadNamespaceContext ctx);
+	void exitBadNamespace(css3Parser.BadNamespaceContext ctx);
+
+	void enterPseudoPage(css3Parser.PseudoPageContext ctx);
+	void exitPseudoPage(css3Parser.PseudoPageContext ctx);
+
+	void enterUnknownDimension(css3Parser.UnknownDimensionContext ctx);
+	void exitUnknownDimension(css3Parser.UnknownDimensionContext ctx);
+
+	void enterBadProperty(css3Parser.BadPropertyContext ctx);
+	void exitBadProperty(css3Parser.BadPropertyContext ctx);
+
+	void enterGoodOperator(css3Parser.GoodOperatorContext ctx);
+	void exitGoodOperator(css3Parser.GoodOperatorContext ctx);
+
+	void enterKnownTerm(css3Parser.KnownTermContext ctx);
+	void exitKnownTerm(css3Parser.KnownTermContext ctx);
+
+	void enterUnused(css3Parser.UnusedContext ctx);
+	void exitUnused(css3Parser.UnusedContext ctx);
+
+	void enterPage(css3Parser.PageContext ctx);
+	void exitPage(css3Parser.PageContext ctx);
+
+	void enterKeyframesRule(css3Parser.KeyframesRuleContext ctx);
+	void exitKeyframesRule(css3Parser.KeyframesRuleContext ctx);
+
+	void enterMediaFeature(css3Parser.MediaFeatureContext ctx);
+	void exitMediaFeature(css3Parser.MediaFeatureContext ctx);
+
+	void enterBadTerm(css3Parser.BadTermContext ctx);
+	void exitBadTerm(css3Parser.BadTermContext ctx);
+
+	void enterBadCharset(css3Parser.BadCharsetContext ctx);
+	void exitBadCharset(css3Parser.BadCharsetContext ctx);
+
+	void enterCombinator(css3Parser.CombinatorContext ctx);
+	void exitCombinator(css3Parser.CombinatorContext ctx);
+
+	void enterFunctionalPseudo(css3Parser.FunctionalPseudoContext ctx);
+	void exitFunctionalPseudo(css3Parser.FunctionalPseudoContext ctx);
+
+	void enterSupportsNegation(css3Parser.SupportsNegationContext ctx);
+	void exitSupportsNegation(css3Parser.SupportsNegationContext ctx);
+
+	void enterClassName(css3Parser.ClassNameContext ctx);
+	void exitClassName(css3Parser.ClassNameContext ctx);
+
+	void enterMedia(css3Parser.MediaContext ctx);
+	void exitMedia(css3Parser.MediaContext ctx);
+
+	void enterTypeNamespacePrefix(css3Parser.TypeNamespacePrefixContext ctx);
+	void exitTypeNamespacePrefix(css3Parser.TypeNamespacePrefixContext ctx);
+
+	void enterGoodImport(css3Parser.GoodImportContext ctx);
+	void exitGoodImport(css3Parser.GoodImportContext ctx);
+
+	void enterSupportsCondition(css3Parser.SupportsConditionContext ctx);
+	void exitSupportsCondition(css3Parser.SupportsConditionContext ctx);
+
+	void enterAttrib(css3Parser.AttribContext ctx);
+	void exitAttrib(css3Parser.AttribContext ctx);
+
+	void enterKnownRuleset(css3Parser.KnownRulesetContext ctx);
+	void exitKnownRuleset(css3Parser.KnownRulesetContext ctx);
+
+	void enterFeatureType(css3Parser.FeatureTypeContext ctx);
+	void exitFeatureType(css3Parser.FeatureTypeContext ctx);
+
+	void enterNegationArg(css3Parser.NegationArgContext ctx);
+	void exitNegationArg(css3Parser.NegationArgContext ctx);
+
+	void enterSelector(css3Parser.SelectorContext ctx);
+	void exitSelector(css3Parser.SelectorContext ctx);
+
+	void enterExpr(css3Parser.ExprContext ctx);
+	void exitExpr(css3Parser.ExprContext ctx);
+
+	void enterSupportsRule(css3Parser.SupportsRuleContext ctx);
+	void exitSupportsRule(css3Parser.SupportsRuleContext ctx);
+
+	void enterUnknownTerm(css3Parser.UnknownTermContext ctx);
+	void exitUnknownTerm(css3Parser.UnknownTermContext ctx);
+
+	void enterWs(css3Parser.WsContext ctx);
+	void exitWs(css3Parser.WsContext ctx);
+
+	void enterUniversal(css3Parser.UniversalContext ctx);
+	void exitUniversal(css3Parser.UniversalContext ctx);
+
+	void enterValue(css3Parser.ValueContext ctx);
+	void exitValue(css3Parser.ValueContext ctx);
+
+	void enterDimension(css3Parser.DimensionContext ctx);
+	void exitDimension(css3Parser.DimensionContext ctx);
+
+	void enterDxImageTransform(css3Parser.DxImageTransformContext ctx);
+	void exitDxImageTransform(css3Parser.DxImageTransformContext ctx);
+
+	void enterCalcValue(css3Parser.CalcValueContext ctx);
+	void exitCalcValue(css3Parser.CalcValueContext ctx);
+
+	void enterSupportsDisjunction(css3Parser.SupportsDisjunctionContext ctx);
+	void exitSupportsDisjunction(css3Parser.SupportsDisjunctionContext ctx);
+
+	void enterMediaQueryList(css3Parser.MediaQueryListContext ctx);
+	void exitMediaQueryList(css3Parser.MediaQueryListContext ctx);
+
+	void enterExpression(css3Parser.ExpressionContext ctx);
+	void exitExpression(css3Parser.ExpressionContext ctx);
+
+	void enterSupportsConjunction(css3Parser.SupportsConjunctionContext ctx);
+	void exitSupportsConjunction(css3Parser.SupportsConjunctionContext ctx);
+
+	void enterKeyframesBlocks(css3Parser.KeyframesBlocksContext ctx);
+	void exitKeyframesBlocks(css3Parser.KeyframesBlocksContext ctx);
+
+	void enterBadOperator(css3Parser.BadOperatorContext ctx);
+	void exitBadOperator(css3Parser.BadOperatorContext ctx);
+
+	void enterGoodProperty(css3Parser.GoodPropertyContext ctx);
+	void exitGoodProperty(css3Parser.GoodPropertyContext ctx);
+
+	void enterAny(css3Parser.AnyContext ctx);
+	void exitAny(css3Parser.AnyContext ctx);
+
+	void enterMediaExpression(css3Parser.MediaExpressionContext ctx);
+	void exitMediaExpression(css3Parser.MediaExpressionContext ctx);
+
+	void enterSupportsDeclarationCondition(css3Parser.SupportsDeclarationConditionContext ctx);
+	void exitSupportsDeclarationCondition(css3Parser.SupportsDeclarationConditionContext ctx);
+
+	void enterSimpleSelectorSequence(css3Parser.SimpleSelectorSequenceContext ctx);
+	void exitSimpleSelectorSequence(css3Parser.SimpleSelectorSequenceContext ctx);
+
+	void enterViewport(css3Parser.ViewportContext ctx);
+	void exitViewport(css3Parser.ViewportContext ctx);
+
+	void enterCalc(css3Parser.CalcContext ctx);
+	void exitCalc(css3Parser.CalcContext ctx);
+}
